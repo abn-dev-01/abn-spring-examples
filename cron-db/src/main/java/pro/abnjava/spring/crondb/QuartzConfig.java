203de7b0f8bfa51ad9e62709ee398db5bc0ddbcb
ANIKIN <abn.dev.01@gmail.com>
2024-03-19 02:29:35 +0300

    cron-db H2 way-2 started

--- cron-db/src/main/java/pro/abnjava/spring/crondb/QuartzConfig.java
+++ cron-db/src/main/java/pro/abnjava/spring/crondb/QuartzConfig.java
@@ -2,15 +2,18 @@ package pro.abnjava.spring.crondb;
 
 import javax.sql.DataSource;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 @Configuration
+@RequiredArgsConstructor
 public class QuartzConfig {
+    private final DataSource dataSource;
 
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean(DataSource dataSource) {
+    public SchedulerFactoryBean schedulerFactoryBean() {
         SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
         schedulerFactoryBean.setDataSource(dataSource);
         // Other configurations...
